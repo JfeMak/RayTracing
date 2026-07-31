c2fbc6f47f65f5aa0c848f0421790d8a5c32a20f
Jeff Mak <e_jeffmak@hotmail.com>
2026-07-31 06:17:33 -0400

    Diffuse materials, shadow acne fix, lambertian reflection + readme changes

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 ## Ray Tracing Project
+
+**Windows:**
+```shell
+$ cmake -B build
+$ cmake --build build --config Release
+$ build\Release\inOneWeekend.exe > image.ppm
+```
+
+**Mac:**
+```shell
+$ cmake -B build/Release -DCMAKE_BUILD_TYPE=Release
+$ cmake --build build/Release
+$ build/Release/inOneWeekend > image.ppm
+```
+
+## Acknowledgements & Credits
+
+This project is a C++ implementation built by closely following the [*Ray Tracing in One Weekend*](https://raytracing.github.io/) book series by **Peter Shirley**, **Trevor David Black**, and **Steve Hollasch**.
--- src/color.h
+++ src/color.h
@@ -8,10 +8,27 @@
 
 using color = vec3;
 
+// Programs assume that an image is gamma corrected before being written into an image file (0 & 1 values have a transformation applied)
+// Linear space (data written w/o being transformed), gamma space (images that are transformed)
+// Gamma 2: Take the power from gamma to linear (square)
+// Linear -> Gamma: inverse of gamma 2 (square root)
+inline double linear_to_gamma(double linear_component) {
+    if (linear_component > 0) {
+        return std::sqrt(linear_component);
+    }
+
+    return 0;
+}
+
 void write_color(std::ostream& out, const color& pixel_color) {
     auto r = pixel_color.x();
     auto g = pixel_color.y();
     auto b = pixel_color.z();
+
+    // Apply a linear to gamma transform for gamma 2
+    r = linear_to_gamma(r);
+    g = linear_to_gamma(g);
+    b = linear_to_gamma(b);
 
     // Translate the [0,1] component values to the byte range [0,255].
     static const interval intensity(0.000, 0.999);
